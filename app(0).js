e9d47cfdd95b408ab535180fcff6e664df0a8925
Andrey <65175184+DoroshukA@users.noreply.github.com>
2020-06-18 11:14:43 +0300

    Add files via upload

---
+++ app(0).js
@@ -0,0 +1,23 @@
+// Find elements.Do something
+
+// $(selector).method();
+
+{/* <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> */}
+
+// $(el)
+// $("*")
+// $("div")
+// $("#test")
+// $(".test")
+// $("div#test")
+// $("div.test")
+// $("div"*)
+// $("div span")
+// $("div>span")
+// $("div+img")
+// $("[checked]")
+// $("[type='button']")
+
+// $("p").find("span").css("color", "red");
+
+$("a[href^='https://']").attr("target","_blank");
